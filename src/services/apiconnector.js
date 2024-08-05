16ffe918be4f43120bccf27777599651830a2aa8
Priyam Gupta <apple@Apples-MacBook-Pro.local>
2024-08-05 21:31:42 +0530

    deployment fix

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -5,6 +5,8 @@ export const axiosInstance = axios.create({});
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
     method: `${method}`,
+
+    
     url: `${url}`,
     data: bodyData ? bodyData : null,
     headers: headers ? headers : null,
